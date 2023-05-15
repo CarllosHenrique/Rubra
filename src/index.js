7e2d88b30c15f0f582c409afca2f6c9cf2f9b548
CarllosHenrique <crick.lucas@gmail.com>
2023-05-15 17:41:11 -0300

    Adicionando estrutura desktop

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const { spawn } = require('child_process')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+    }
+  })
+
+  mainWindow.loadURL('http://localhost:3000');
+
+  const appProcess = spawn('bundle', ['exec', 'ruby', 'main.rb'])
+  appProcess.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`)
+  })
+  appProcess.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`)
+  })
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
